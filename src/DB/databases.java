f8de8fa348a76d460909b316d0208d7da35e4d36
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-02-15 22:46:24 +0900

    DB연동

--- src/DB/databases.java
+++ src/DB/databases.java
@@ -17,5 +17,18 @@ public class databases {
 		}
 	}
 	
+	public static void close(Connection con) {
+		try {if(con!=null) {con.close();}}catch(Exception e) {e.printStackTrace();}
+	}
+	public static void close(Statement stmt) {
+		try{if(stmt != null) {stmt.close();}}catch(Exception e) {e.printStackTrace();}
+	}
+	public static void close(PreparedStatement pstmt) {
+		try {if(pstmt != null) {pstmt.close();}}catch(Exception e) {e.printStackTrace();}
+	}
+	public static void close(ResultSet rs) {
+		try {if(rs!=null) {rs.close();}}catch(Exception e) {e.printStackTrace();}
+	}
+	
 
 }
