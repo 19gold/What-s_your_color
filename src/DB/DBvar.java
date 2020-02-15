6966483665a5112b5c658c5f48e180b1658c2686
Kang JooYoung <s2018s21@e-mirim.hs.kr>
2020-02-15 22:47:31 +0900

    DB변수들

---
+++ src/DB/DBvar.java
@@ -0,0 +1,35 @@
+package DB;
+
+public class DBvar {
+	private String name;//¿Ã∏ß_member
+	private String id;//id_member;
+	private String psw;//psw_member;
+	private String email;//email_member;
+	
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(String id) {
+		this.id=id;
+	}
+	public String getId(){
+		return id;
+	}
+	public void setPsw(String psw) {
+		this.psw=psw;
+	}
+	public String getPsw() {
+		return psw;
+	}
+	public void setEmail(String email) {
+		this.email=email;
+	}
+	public String getEmail() {
+		return email;
+	}
+	
+}
